b2f0839b55edb1059ff877b65ec6f8e1f03cae79
Francisco oltra <foltra@gmail.com>
2017-02-23 19:30:04 -0600

    change to use loop count

--- hamming/hamming.lisp
+++ hamming/hamming.lisp
@@ -7,11 +7,5 @@
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands." 
   (if (eql (length dna1)(length dna2))
-      (let ((c 0)) ;; set count on 0
         (loop for i from 0 below (length dna1)
-              when (char-not-equal (char dna1 i)(char dna2 i))
-                do (incf c)
-              )
-        c)
-      nil)
-  )
+             count (char-not-equal (char dna1 i)(char dna2 i)))))
