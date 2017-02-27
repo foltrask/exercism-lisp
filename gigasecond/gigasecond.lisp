032fa22041e62c141472c1cbae7d3f89c949de8f
Francisco oltra <foltra@gmail.com>
2017-02-27 08:17:46 -0600

    solved with external class

---
+++ gigasecond/gigasecond.lisp
@@ -0,0 +1,17 @@
+(in-package #:cl-user)
+(defpackage #:gigasecond
+  (:use #:cl)
+  (:use #:simple-date-time)
+  (:export #:from))
+(in-package #:gigasecond)
+
+(defparameter *gigasecond* (expt 10 9))
+(defun from (year month day hour minute second)
+  (let ((giga-date (second+ (make-date-time year month day hour minute second)
+                           *gigasecond*)))
+    (list (year-of giga-date)
+          (month-of giga-date)
+          (day-of giga-date)
+          (hour-of giga-date)
+          (minute-of giga-date)
+          (second-of giga-date))))
